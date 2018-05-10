ff1a057b211bbb12adbc0da2bb5f100c816c144d
wing-0 <38071649+wing-0@users.noreply.github.com>
2018-05-10 17:51:10 +0200

    Add script for handin 3.1 and start work on it. Until d) it works fine (hopefully), but issues are present in the d) part. No parts beyond that implemented

---
+++ Handin3/Handin3_1.m
@@ -0,0 +1,78 @@
+%% Initialize
+clearvars
+close all
+
+% Node link incidence matrix
+B = load('traffic.mat','-ASCII');
+
+% Link maximum flow capacities
+Cap = load('capacities.mat','-ASCII');
+
+% Link minimum travel times
+l = load('traveltime.mat','-ASCII');
+
+% Construct weight matrices with travel times and capacities as weight
+Wl = zeros(size(B,1));
+Wcap = Wl;
+
+% Iterate over all links in B and set W('tail','head') to the
+% minimum travel time/maximum flow capacity of each link
+for k = 1:size(B,2)
+    Wl(B(:,k)==1,B(:,k)==-1) = l(k);
+    Wcap(B(:,k)==1,B(:,k)==-1) = Cap(k);
+end
+
+%% a) and b) - calculate shortest path and maximum flow between node 1-17
+[DIST,~,~] = graphshortestpath(sparse(Wl),1,17);
+[M,~,~] = graphmaxflow(sparse(Wcap),1,17);
+
+fprintf('Shortest path node 1-17: %0.2f h \n',DIST)
+fprintf('Maximum flow node 1-17: %0.0f vehicles/h \n',M)
+
+%% c) - External inflow and outflow
+f = load('flow.mat','-ASCII');
+
+% Cacluate external flows, and extract positive and negative flows as in-
+% and outflows
+ext = B*f;
+ext_in = ext(ext>0);
+ext_out = -ext(ext<0);
+
+fprintf('----------------------------\n')
+fprintf('External inflow \n')
+fprintf('\t%-6s %-8s \n','Node','Inflow')
+fprintf('\t%-6.0f %-8.0f \n',[find(ext>0) ext_in]')
+
+fprintf('External outflow \n')
+fprintf('\t%-6s %-8s \n','Node','Outflow')
+fprintf('\t%-6.0f %-8.0f \n',[find(ext<0) ext_out]')
+fprintf('----------------------------\n')
+
+%% d) - Social optimum
+
+% Exogenous inflows
+lambda = zeros(size(B,1),1);
+lambda(1) = ext(1);
+
+% Exogenous outflows
+mu = zeros(size(B,1),1);
+mu(end) = -ext(1);
+
+% Net exogenous flow
+nu = lambda - mu;
+
+% Optimize using cvx
+cvx_begin
+    variable f(size(B,2))
+    minimize sum( l.*Cap./( ones(size(B,2),1) - f./Cap ) - l.*Cap )
+    subject to
+        B*f == nu
+        f >= 0
+cvx_end
+
+
+
+
+
+
+
