fade1665c44b629433c0809e4fbeb9f3f426dc27
wing-0 <38071649+wing-0@users.noreply.github.com>
2018-05-11 12:21:48 +0200

    Add more to the script for handin 3.1. Solves until part f).

--- Handin3/Handin3_1.m
+++ Handin3/Handin3_1.m
@@ -38,7 +38,7 @@ ext = B*f;
 ext_in = ext(ext>0);
 ext_out = -ext(ext<0);
 
-fprintf('----------------------------\n')
+fprintf('--------------------------------------------------------------\n')
 fprintf('External inflow \n')
 fprintf('\t%-6s %-8s \n','Node','Inflow')
 fprintf('\t%-6.0f %-8.0f \n',[find(ext>0) ext_in]')
@@ -46,7 +46,7 @@ fprintf('\t%-6.0f %-8.0f \n',[find(ext>0) ext_in]')
 fprintf('External outflow \n')
 fprintf('\t%-6s %-8s \n','Node','Outflow')
 fprintf('\t%-6.0f %-8.0f \n',[find(ext<0) ext_out]')
-fprintf('----------------------------\n')
+fprintf('--------------------------------------------------------------\n')
 
 %% d) - Social optimum
 
@@ -56,20 +56,64 @@ lambda(1) = ext(1);
 
 % Exogenous outflows
 mu = zeros(size(B,1),1);
-mu(end) = -ext(1);
+mu(end) = ext(1);
 
 % Net exogenous flow
 nu = lambda - mu;
 
 % Optimize using cvx
 cvx_begin
-    variable f(size(B,2))
-    minimize sum( l.*Cap./( ones(size(B,2),1) - f./Cap ) - l.*Cap )
+    variable f_so(size(B,2))
+    minimize sum( l.*Cap.*inv_pos( ones(size(B,2),1) - f_so./Cap ) - l.*Cap )
     subject to
-        B*f == nu
-        f >= 0
+        B*f_so == nu
+        f_so >= 0
 cvx_end
 
+fprintf('--------------------------------------------------------------\n')
+fprintf('Social optimum flows \n')
+fprintf('\t%-6s %-8s \n','Link','Flow')
+fprintf('\t%-6.0f %-8.0f \n',[(1:size(B,2))' f_so]')
+fprintf('--------------------------------------------------------------\n')
+
+%% e) - Wardrop equilibrium
+
+% Primitive of the delay function is -C*l*log(c-f)
+
+% Optimize using cvx
+cvx_begin
+    variable f_uo(size(B,2))
+    minimize sum( -Cap.*l.*log(Cap-f_uo) )
+    subject to
+        B*f_uo == nu
+        f_uo >= 0
+cvx_end
+
+fprintf('--------------------------------------------------------------\n')
+fprintf('\t%-6s %-10s %-10s \n','Link','SO flow', 'UO flow')
+fprintf('\t%-6.0f %-10.0f %-10.0f \n',[(1:size(B,2))' f_so f_uo]')
+fprintf('--------------------------------------------------------------\n')
+
+%% f) - Tolls
+
+% Derivative of the delay function is C*l/(c-f)^2
+
+% Toll vector
+w = f_so.*Cap.*l./(Cap-f_so).^2;
+
+% Optimize using cvx
+cvx_begin
+    variable f_t(size(B,2))
+    minimize sum( -Cap.*l.*log(Cap-f_t) + w.*f_t )
+    subject to
+        B*f_t == nu
+        f_t >= 0
+cvx_end
+
+fprintf('--------------------------------------------------------------\n')
+fprintf('\t%-6s %-10s %-10s %-10s \n','Link','SO flow', 'UO flow', 'UO w/tolls')
+fprintf('\t%-6.0f %-10.0f %-10.0f %-10.0f \n',[(1:size(B,2))' f_so f_uo f_t]')
+fprintf('--------------------------------------------------------------\n')
 
 
 
