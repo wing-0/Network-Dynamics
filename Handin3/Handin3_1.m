ed0c12c70ff77048c003cbaafb483dc52c933c0c
wing-0 <38071649+wing-0@users.noreply.github.com>
2018-05-12 12:57:54 +0200

    Finish script for handin3.1

--- Handin3/Handin3_1.m
+++ Handin3/Handin3_1.m
@@ -26,8 +26,12 @@ end
 [DIST,~,~] = graphshortestpath(sparse(Wl),1,17);
 [M,~,~] = graphmaxflow(sparse(Wcap),1,17);
 
+fprintf('--------------------------------------------------------------\n')
+fprintf('Results a), b) \n')
+fprintf('--------------------------------------------------------------\n')
 fprintf('Shortest path node 1-17: %0.2f h \n',DIST)
 fprintf('Maximum flow node 1-17: %0.0f vehicles/h \n',M)
+fprintf('--------------------------------------------------------------\n')
 
 %% c) - External inflow and outflow
 f = load('flow.mat','-ASCII');
@@ -38,6 +42,8 @@ ext = B*f;
 ext_in = ext(ext>0);
 ext_out = -ext(ext<0);
 
+fprintf('--------------------------------------------------------------\n')
+fprintf('Results c) \n')
 fprintf('--------------------------------------------------------------\n')
 fprintf('External inflow \n')
 fprintf('\t%-6s %-8s \n','Node','Inflow')
@@ -71,6 +77,8 @@ cvx_begin
 cvx_end
 
 fprintf('--------------------------------------------------------------\n')
+fprintf('Results d) \n')
+fprintf('--------------------------------------------------------------\n')
 fprintf('Social optimum flows \n')
 fprintf('\t%-6s %-8s \n','Link','Flow')
 fprintf('\t%-6.0f %-8.0f \n',[(1:size(B,2))' f_so]')
@@ -89,6 +97,8 @@ cvx_begin
         f_uo >= 0
 cvx_end
 
+fprintf('--------------------------------------------------------------\n')
+fprintf('Results e) \n')
 fprintf('--------------------------------------------------------------\n')
 fprintf('\t%-6s %-10s %-10s \n','Link','SO flow', 'UO flow')
 fprintf('\t%-6.0f %-10.0f %-10.0f \n',[(1:size(B,2))' f_so f_uo]')
@@ -111,11 +121,46 @@ cvx_begin
 cvx_end
 
 fprintf('--------------------------------------------------------------\n')
+fprintf('Results f) \n')
+fprintf('--------------------------------------------------------------\n')
 fprintf('\t%-6s %-10s %-10s %-10s \n','Link','SO flow', 'UO flow', 'UO w/tolls')
 fprintf('\t%-6.0f %-10.0f %-10.0f %-10.0f \n',[(1:size(B,2))' f_so f_uo f_t]')
 fprintf('--------------------------------------------------------------\n')
 
 %% g) - Total additional delay
+
+% The total additional delay can be written as l*f^2/(C-f)
+
+% Optimize using cvx (system optimum)
+cvx_begin
+    variable f_so2(size(B,2))
+    minimize sum( l.*quad_over_lin( f_so2, (Cap-f_so2), 0 ) )
+    subject to
+        B*f_so2 == nu
+        f_so2 >= 0
+cvx_end
+
+% Constructing tolls for Wardrop equilibrium coinciding with system optimum
+% Derivative of the cost function is f*l*(2C-f)/(C-f)^2
+w2 = f_so2.*l.*(2*Cap-f_so2)./(Cap-f_so2).^2 - l./(1-f_so2./Cap);
+
+% Optimize using cvx (Wardrop equilibrium w/tolls)
+% Primitive of the delay function used as D_e, but the cost function is
+% used in the tolls
+cvx_begin
+    variable f_t2(size(B,2))
+    minimize sum( -Cap.*l.*log(Cap-f_t2) + w2.*f_t2 )
+    subject to
+        B*f_t2 == nu
+        f_t2 >= 0
+cvx_end
+
+fprintf('--------------------------------------------------------------\n')
+fprintf('Results g) \n')
+fprintf('--------------------------------------------------------------\n')
+fprintf('\t%-6s %-10s  %-10s \n','Link','SO flow', 'UO w/tolls')
+fprintf('\t%-6.0f %-10.0f  %-10.0f \n',[(1:size(B,2))' f_so2 f_t2]')
+fprintf('--------------------------------------------------------------\n')
 
 
 
